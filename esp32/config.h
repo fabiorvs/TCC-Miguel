d88d42f3ed4c89162aafe462d637a2c159c1721f
Fabio <fabiorvs@outlook.com>
2025-04-29 20:33:07 -0300

    config

--- esp32/config.h
+++ esp32/config.h
@@ -3,7 +3,7 @@
 
 // Configurações de WiFi
 const char* WIFI_SSID = "AP03";            // Nome da rede WiFi
-const char* WIFI_PASSWORD = "fabio123eu";  // Senha do WiFi
+const char* WIFI_PASSWORD = "123";  // Senha do WiFi
 
 // Configuração da API (Servidor HTTP)
 const char* SERVER_HOST = "192.168.15.42";                      // IP do servidor
